2024e3f77ee2f609b75a0ac8e3e5b90fc47d80d1
Vlad <29-vinitskiy-vo@mail.ru>
2022-03-20 15:36:12 +0700

    добывил радиокнопки и клавиатуру

--- fp/src/components/HelloWorld.vue
+++ fp/src/components/HelloWorld.vue
@@ -1,74 +1,149 @@
 <template>
   <div class="hello">
-
     <h1>{{ msg }}</h1>
-    <hr>
-    <button @click = "doThat">Счелкни</button>
-    <hr>
+
+    <hr />
 
     <div class="display">
-      <input v-model.number ="operand1">
-      <input v-model.number ="operand2">
+      <input v-model.number="operand1" />
+      <input v-model.number="operand2" />
       = {{ result }}
     </div>
 
     <div class="keyboard">
-      <button @click="result = operand1 + operand2" >+</button>
-      <button @click="result = operand1 - operand2">-</button>
-      <button @click="divide(operand1, operand2)">/</button>
-      <button @click="multiply(operand1, operand2)">*</button>
-      <button @click="degree(operand1, operand2)">**</button>
-      <button @click="integer(operand1, operand2)">//</button>
+      <button
+        v-for="operand in operands"
+        :key="operand"
+        :title="operand"
+        @click="calculate(operand)"
+      >
+        {{ operand }}
+      </button>
     </div>
 
+    <div>
+      <input type="checkbox" id="checkbox" v-model="checked" />
+      <label for="checkbox">Отобразить экранную клавиатуру</label>
+    </div>
+
+    <div v-show="checked">
+      <button v-for="btn in btns" :key="btn">
+        {{ btn }}
+      </button>
+    </div>
+
+    <div>
+      
+        <input
+          type="radio"
+          id="one"
+          
+          name="operand"
+          v-model="operand1"
+          @click="selectOperand1()"
+        />
+        <label for="one">Операнд 1</label>
+
+        <input
+          type="radio"
+          id="two"
+          
+          name="operand"
+          v-model="operand2"
+          @click="selectOperand2()"
+        />
+        <label for="two">Операнд 2</label>
+      
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      message: 'Hello Vue',
-      operand1: 0,
-      operand2: 0,
+      message: "Hello Vue",
+      operands: ["+", "-", "*", "/", "**", "//"],
+      btns: [0, "1", "2", "3", "4", "5", "6", "7", "8", "9", "<--"],
+      operand1: "",
+      operand2: "",
       result: 0,
-    }
+      checked: "",
+      selectOp1: false,
+      selectOp2: false,
+    };
   },
   methods: {
-    doThat(){
-      console.log('счелк')
+    selectOperand1(){
+      this.selectOp1 = true
+      this.selectOp2 = false
     },
-    divide (op1, op2){
-      this.result = op1 / op2
+    selectOperand2(){
+      this.selectOp1 = false
+      this.selectOp2 = true
     },
-    multiply (op1, op2) {
-      this.result = op1 * op2
+    doThat() {
+      console.log("счелк");
     },
-    degree (op1, op2) {
-      this.result = Math.pow(op1, op2)
-      if(this.result < 0){
-        this.result = this.result * -1
+    calculate(operand) {
+      switch (operand) {
+        case "+":
+          this.summ();
+          break;
+        case "-":
+          this.subtraction();
+          break;
+        case "*":
+          this.multiply();
+          break;
+        case "/":
+          this.divide();
+          break;
+        case "**":
+          this.degree();
+          break;
+        case "//":
+          this.integer();
+          break;
       }
     },
-    integer (op1, op2) {
-      if(op1/op2 >= 0){
-        this.result = Math.floor(op1/op2)
-        if(this.result < 0){
-        this.result = this.result * -1
+    summ() {
+      this.result = this.operand1 + this.operand2;
+    },
+    subtraction() {
+      this.result = this.operand1 - this.operand2;
+    },
+    divide() {
+      this.result = this.operand1 / this.operand2;
+    },
+    multiply() {
+      this.result = this.operand1 * this.operand2;
+    },
+    degree() {
+      this.result = Math.pow(this.operand1, this.operand2);
+      if (this.result < 0) {
+        this.result = this.result * -1;
       }
+    },
+    integer() {
+      if (this.operand1 / this.operand2 >= 0) {
+        this.result = Math.floor(this.operand1 / this.operand2);
+        if (this.result < 0) {
+          this.result = this.result * -1;
+        }
       } else {
-        this.result = Math.ceil(op1/op2)
-        if(this.result < 0){
-        this.result = this.result * -1
+        this.result = Math.ceil(this.operand1 / this.operand2);
+        if (this.result < 0) {
+          this.result = this.result * -1;
+        }
       }
-      }
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
